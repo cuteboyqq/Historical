1ad242df7bad0ed54a973edf94135796f2e82f09
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2024-07-22 18:24:13 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from engine.BaseDataset import BaseDataset
 from task.Historical import *
 from config.config import *
 
@@ -7,7 +6,12 @@ from config.config import *
 if __name__=="__main__":
     args = get_args()
     history = Historical(args)
-    # Extract data from both files
-    frame_ids_1, distances_1 = history.Draw_AI_result_to_images()
 
-    
+    DRAW_AI_RESULT_ON_IMAGES = True
+    PLOTDISTANCE = False
+
+    if DRAW_AI_RESULT_ON_IMAGES:
+        frame_ids_1, distances_1 = history.draw_AI_result_to_images()
+
+    if PLOTDISTANCE:
+        history.compare_distance_in_multiple_csv_file()
