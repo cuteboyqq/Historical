5abc14908425d9fd5aaa0c78b82113f2dca293f5
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2024-07-30 01:04:38 +0800

    Add files via upload

--- using_socket.py
+++ using_socket.py
@@ -11,32 +11,74 @@ def process_json_log(json_log):
         print("Received JSON:", json.dumps(log_data, indent=4))  # Print formatted JSON
 
         frame_ID = list(log_data["frame_ID"].keys())[0]  # Extract the first frame_ID key
+        
         detect_objs = log_data["frame_ID"][frame_ID]["detectObj"]["VEHICLE"]
         tailing_objs = log_data["frame_ID"][frame_ID]["tailingObj"]
+        vanishline_objs = log_data["frame_ID"][frame_ID]["vanishLineY"]
 
         image_path = f"assets/images/2024-7-31-15-57/RawFrame_{frame_ID}.png"
         print(image_path)
         image = cv2.imread(image_path)
+        cv2.putText(image, 'frame_ID:'+str(frame_ID), (10,10), cv2.FONT_HERSHEY_SIMPLEX,0.45, (0, 255, 255), 1, cv2.LINE_AA)
+        for obj in vanishline_objs:
+            vanishlineY = obj["vanishlineY"]
+            x2 = image.shape[1]
+            cv2.line(image, (0, vanishlineY), (x2, vanishlineY), (0, 255, 255), thickness=1)
+            cv2.putText(image, 'VanishLineY:' + str(round(vanishlineY,3)), (10,30), cv2.FONT_HERSHEY_SIMPLEX,0.45, (0, 255, 255), 1, cv2.LINE_AA)
 
+        for obj in tailing_objs:
+            tailingObj_x1, tailingObj_y1 = obj["tailingObj.x1"], obj["tailingObj.y1"]
+            tailingObj_x2, tailingObj_y2 = obj["tailingObj.x2"], obj["tailingObj.y2"]
+            distance = obj["tailingObj.distanceToCamera"]
+            label = obj["tailingObj.label"]
+            distance_to_camera = obj['tailingObj.distanceToCamera']
+            tailingObj_id = obj['tailingObj.id']
+            tailingObj_label = obj['tailingObj.label']
+            # cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            im = image
+            showtailobjBB_corner = True
+            if showtailobjBB_corner:
+                top_left = (tailingObj_x1, tailingObj_y1)
+                bottom_right = (tailingObj_x2, tailingObj_y2)
+                top_right = (tailingObj_x2,tailingObj_y1)
+                bottom_left = (tailingObj_x1,tailingObj_y2) 
+                BB_width = abs(tailingObj_x2 - tailingObj_x1)
+                BB_height = abs(tailingObj_y2 - tailingObj_y1)
+                divide_length = 5
+                thickness = 3
+                color = (0,255,255)
+                # Draw each side of the rectangle
+                cv2.line(im, top_left, (top_left[0]+int(BB_width/divide_length), top_left[1]), color, thickness)
+                cv2.line(im, top_left, (top_left[0], top_left[1] + int(BB_height/divide_length)), color, thickness)
+
+                cv2.line(im, bottom_right,(bottom_right[0] - int(BB_width/divide_length),bottom_right[1]), color, thickness)
+                cv2.line(im, bottom_right,(bottom_right[0],bottom_right[1] - int(BB_height/divide_length) ), color, thickness)
+
+
+                cv2.line(im, top_right, ((top_right[0]-int(BB_width/divide_length)), top_right[1]), color, thickness)
+                cv2.line(im, top_right, (top_right[0], (top_right[1]+int(BB_height/divide_length))), color, thickness)
+
+                cv2.line(im, bottom_left, ((bottom_left[0]+int(BB_width/divide_length)), bottom_left[1]), color, thickness)
+                cv2.line(im, bottom_left, (bottom_left[0], (bottom_left[1]-int(BB_height/divide_length))), color, thickness)
+            else:
+                cv2.rectangle(im, (tailingObj_x1, tailingObj_y1), (tailingObj_x2, tailingObj_y2), color=(0,255,255), thickness=2)
+                cv2.putText(image, f"{label} ({distance:.2f}m)", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+            cv2.putText(im, f'{tailingObj_label} ID:{tailingObj_id}', (tailingObj_x1, tailingObj_y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 255), 1, cv2.LINE_AA)
+            cv2.putText(im, 'Distance:' + str(round(distance_to_camera,3)) + 'm', (tailingObj_x1, tailingObj_y1-25), cv2.FONT_HERSHEY_SIMPLEX,0.45, (0, 255, 255), 1, cv2.LINE_AA)
+        
         for obj in detect_objs:
             x1, y1 = obj["detectObj.x1"], obj["detectObj.y1"]
             x2, y2 = obj["detectObj.x2"], obj["detectObj.y2"]
             confidence = obj["detectObj.confidence"]
             label = obj["detectObj.label"]
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(image, f"{label} ({confidence:.2f})", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        for obj in tailing_objs:
-            x1, y1 = obj["tailingObj.x1"], obj["tailingObj.y1"]
-            x2, y2 = obj["tailingObj.x2"], obj["tailingObj.y2"]
-            distance = obj["tailingObj.distanceToCamera"]
-            label = obj["tailingObj.label"]
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.putText(image, f"{label} ({distance:.2f}m)", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
+            if tailingObj_x1!=x1 and tailingObj_y1!=y1:
+                cv2.rectangle(image, (x1, y1), (x2, y2), (255, 200, 0), 1)
+                cv2.putText(image, f"{label} ({confidence:.2f})", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 200, 0), 1)
+        image = cv2.resize(image, (1280, 720), interpolation=cv2.INTER_AREA)
         cv2.imshow("Annotated Image", image)
         cv2.waitKey(1)  # Display the image for a short time
-
+        cv2.imwrite(f'AI_result_images/frame_{frame_ID}.jpg',image)
     except KeyError as e:
         print(f"KeyError: {e} - The key might be missing in the JSON data.")
     except json.JSONDecodeError as e:
@@ -71,7 +113,7 @@ def get_local_ip():
     local_ip = socket.gethostbyname(hostname)
     return local_ip
 
-
+import os
 def start_server():
     host = '192.168.1.10'  # Bind to localhost
     port = 5000  # Non-privileged port number
@@ -90,7 +132,7 @@ def start_server():
 
     server_socket.listen(5)
     print(f"Server started on {host}:{port}")
-
+    os.makedirs('AI_result_images',exist_ok=True)
     while True:
         client_socket, addr = server_socket.accept()
         print(f"Connection from {addr}")
